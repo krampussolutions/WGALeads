25e21bd575764b479e1ef32d13ae4e36f3d1036b
Matthew Poteet <krampussolutions@gmail.com>
2026-03-03 10:51:10 -0500

    Add Meta Pixel (PageView) tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,10 +62,12 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const PIXEL_ID = "1381934923685815";
+
   return (
     <html lang="en">
       <head>
-        {/* Step 5: Schema markup for SEO */}
+        {/* Schema markup for SEO */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -83,6 +85,33 @@ export default function RootLayout({
       </head>
 
       <body>
+        {/* Meta Pixel */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${PIXEL_ID}');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
+
         <div className="min-h-screen">{children}</div>
       </body>
     </html>
